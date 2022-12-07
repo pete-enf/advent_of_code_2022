d512e5f3ee2a6712ea3a8c8fdc4a680fc71a6a6e
Peter Hyvonen <pete@enfabrica.net>
2022-12-07 03:31:48 +0000

    finds greatest value

--- day1/day1.go
+++ day1/day1.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+    "bufio"
     "fmt"
-    "io/ioutil"
     "log"
+    "os"
+    "strconv"
 )
 
 func main() {
-	content, err := ioutil.ReadFile("elf_snacks.txt")
-	
+	lines , err := os.Open("elf_snacks.txt")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("failed to open")
 	}
-	fmt.Println(string(content))
+	scanner := bufio.NewScanner(lines)
+	scanner.Split(bufio.ScanLines)
+	var text []string
+	for scanner.Scan() {
+		text = append(text, scanner.Text())
+	}
+	lines.Close()
+	var lastVar int = 0
+	for _, line := range text {
+	    intVar, err := strconv.Atoi(line)
+	    if intVar > lastVar {
+		lastVar = intVar
+	    }
+	    fmt.Println(intVar, lastVar, err)
+	}
 }
